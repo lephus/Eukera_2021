20f0eb63b24be33eeae187aba3826cb1b60cfc46
LeHuuPhu <phulealali@gmail.com>
2021-10-20 21:36:54 +0700

    first commit

--- backend/summaryText.py
+++ backend/summaryText.py
@@ -3,14 +3,25 @@ from database import connect
 
 summarizer = pipeline("summarization")
 
-dataPost = list(connect.db.posts.find({}))
+dataPost = list(connect.db.tb_posts.find({}))
 for x in dataPost:
     if(x['summary'] == ""):
         print("Summary Text Record ID: "+str(x['idPost']))
-        value = (summarizer(x['description'], max_length=200, min_length=50, do_sample=False))
+        value = ""
+        print(len(x['description']))
+        if( len(x['description']) <= 1000):
+            res         = (summarizer(x['description'], max_length=200, min_length=50, do_sample=False))
+            value       += res[0]['summary_text']
+        else: 
+            dataInput   = x['description']
+            res         = (summarizer(dataInput[0:1000], max_length=200, min_length=50, do_sample=False))
+            value       += res[0]['summary_text']
+            c           = 1000 - len(dataInput)
+            res         = (summarizer(dataInput[c], max_length=200, min_length=50, do_sample=False))
+            value       += res[0]['summary_text']
         tmp  = {
             "idPost"        : x['idPost'],
-            "idUser"        : x['title'],
+            "idAuthor"      : x['idAuthor'],
             "title"         : x['title'],
             "viewer"        : x['viewer'],
             "idCategory"    : x['idCategory'],
@@ -18,8 +29,9 @@ for x in dataPost:
             "tag"           : x['tag'],
             "description"   : x['description'],
             "content"       : x['content'],
-            "summary"       : str(value[0]['summary_text']),
+            "summary"       : str(value),
             "createAt"      : x['createAt'],
-            "updateAt"      : x['updateAt']
+            "updateAt"      : x['updateAt'],
+            "thumbnail"     : x['thumbnail'],
         }
-        connect.db.posts.update_one(x,{ "$set": tmp })
+        connect.db.tb_posts.update_one(x,{ "$set": tmp })
